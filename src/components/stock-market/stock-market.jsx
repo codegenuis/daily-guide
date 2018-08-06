8b9477107435573d89a2f064993ac61686f2d840
codegenuis <charlesosegi@gmail.com>
2018-08-06 17:53:52 +0100

    implemented the weather and news section

--- src/components/stock-market/stock-market.jsx
+++ src/components/stock-market/stock-market.jsx
@@ -5,14 +5,30 @@ class StockMarket extends Component {
   constructor() {
     super();
     this.state = {
+      country: '',
+      articles: [],
     };
+  }
+
+  componentWillMount() {
+    fetch('https://api.ipdata.co/?api-key=9f3e22a7f0037eebecc55989c82823c27cddca523bcd59eff04a95a1')
+      .then(res => res.json())
+      .then((location) => {
+        this.setState({ country: location.country_code.toLowerCase() });
+        fetch(`https://newsapi.org/v2/top-headlines?country=${this.state.country}&category=business&apiKey=125cae3691a140489efa2b69a8d31d88`)
+          .then(res => res.json())
+          .then((response) => {
+            console.log(response);
+            this.setState({ articles: response.articles });
+          });
+      });
   }
 
   render() {
     return (
       <Card>
         <CardBody>
-          <p>Stock Market</p>
+          {this.state.articles.map(x => <p>{x.title}</p>)}
         </CardBody>
       </Card>
     );
--- src/components/weather/weather.jsx
+++ src/components/weather/weather.jsx
@@ -5,14 +5,42 @@ class Weather extends Component {
   constructor() {
     super();
     this.state = {
+      temp: '',
+      latitude: '',
+      longitude: '',
+      icon: '',
+      description: '',
     };
   }
 
+  componentWillMount() {
+    fetch('https://api.ipdata.co/?api-key=9f3e22a7f0037eebecc55989c82823c27cddca523bcd59eff04a95a1')
+      .then(res => res.json())
+      .then((location) => {
+        // console.log(location.latitude);
+        this.setState({ latitude: location.latitude, longitude: location.longitude });
+        console.log(this.state.longitude);
+        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${this.state.latitude}&lon=${this.state.longitude}&appid=9bc3219af03414a42ba6f7ae4b89889b`)
+          .then(response => response.json())
+          .then((data) => {
+            console.log(data);
+            this.setState({
+              temp: data.main.temp,
+              description: data.weather[0].description,
+              icon: data.weather[0].icon,
+            });
+          });
+      });
+  }
+
   render() {
+    // const { weather } = this.state;
     return (
       <Card>
         <CardBody>
-          <p>Weather</p>
+          <img src={`http://openweathermap.org/img/w/${this.state.icon}.png`} alt="imag" />
+          <p>{this.state.description}</p>
+          <p>{this.state.temp}</p>
         </CardBody>
       </Card>
     );
