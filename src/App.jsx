3f69369c2819a9f59745a8f5477de7057235d9de
codegenuis <charlesosegi@gmail.com>
2018-08-23 06:21:32 +0100

    implemented loading gif for the index page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import PastEvents from './components/past-events/past-events';
 import Weather from './components/weather/weather';
 import StockMarket from './components/stock-market/stock-market';
+import ExchangeRate from './components/exchange-rates/exchange-rates';
 import './App.css';
+import load from './loading.svg';
 
-const App = () => (
-  <div className="">
-    <div className="row">
-      <div className="col-md-3">
-        <PastEvents />
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ loading: false });
+    }, 2000);
+  }
+
+  render() {
+    const { loading } = this.state;
+    return (
+      <div>
+        {loading
+          ? <div className="preloader" style={{background: `#fff url(${load}) no-repeat center center`}} /> : null
+        }
+        <div className="row">
+          <div className="col-md-3">
+            <PastEvents />
+          </div>
+          <div className="col-md-3">
+            <Weather />
+          </div>
+          <div className="col-md-3">
+            <StockMarket />
+          </div>
+          <div className="col-md-3">
+            <ExchangeRate />
+          </div>
+        </div>
       </div>
-      <div className="col-md-3">
-        <Weather />
-      </div>
-      <div className="col-md-3">
-        <StockMarket />
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default App;
---
+++ src/components/exchange-rates/exchange-rates.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class ExchangeRate extends Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="card">
+        <p> Exchange Rates Works </p>
+      </div>
+    );
+  }
+}
+
+export default ExchangeRate;
